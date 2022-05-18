13f027ca7e651446c7401afa123265fd294ed148
Simone Lopes <sisilopes20@gmail.com>
2022-05-17 22:18:59 -0300

    Add typescript at project

---
+++ src/components/SkillCard.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  TouchableOpacityProps,
+} from 'react-native';
+
+interface SkillCardProps extends TouchableOpacityProps {
+  skill: string;
+}
+
+export function SkillCard({skill, ...rest}: SkillCardProps) {
+  return (
+    <TouchableOpacity style={styles.buttonSkill} {...rest}>
+      <Text style={styles.textSkill}>{skill}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonSkill: {
+    backgroundColor: '#1f1e25',
+    padding: 15,
+    borderRadius: 50,
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  textSkill: {
+    color: '#fff',
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+});
